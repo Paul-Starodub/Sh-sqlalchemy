1e441b5f73533f31a90b9d09a9ddc669f4353274
Pavlo Starobub <starodub.pavlo@alsodev.md>
2026-01-21 16:29:17 +0200

    Add config.py for database

---
+++ src/config.py
@@ -0,0 +1,24 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    DB_HOST: str
+    DB_PORT: int
+    DB_NAME: str
+    DB_USER: str
+    DB_PASSWORD: str
+
+    @property
+    def DATABASE_URL_asyncpg(self):
+        # postgresql+asyncpg://user:password@host:port/dbname
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+    @property
+    def DATABASE_URL_psycopg(self):
+        # postgresql+psycopg://user:password@host:port/dbname
+        return f"postgresql://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+
+settings = Settings()
